e2a2d42d23ff09a5d4c8ce0d380e42afa92af155
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-10 19:45:03 -0500

    evening

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
+
 const rowdy = require('rowdy-logger')
 const routesReport = rowdy.begin(app)
 const userRoutes = require('./routes/userRoutes')
-const axios = require('axios');
 
-const getData = async () => {
-    return await axios ({
-        url: 'https://api.dictionaryapi.dev/api/v2/entries/en_US/deaf'
-    })
-}
-
-
- (async () => {
-     const definiton = await getData()
-     console.log(definiton.data.meanings)
- })()
 
   app.use(express.json())
   app.use(require('cors')())
+  app.use('/user', userRoutes)
 
 //   app.listen(PORT, () => {
 //     console.log(`Listening on port ${PORT}`)
@@ -30,5 +21,3 @@ app.listen(PORT, () => {
     console.log(`port running on ${PORT}`)
     routesReport.print()
 })
-
-app.use('/user', userRoutes)
