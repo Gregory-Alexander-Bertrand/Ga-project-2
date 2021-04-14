3838374f4eee9774e2f429717ed4e1e8890b382a
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-13 21:38:30 -0500

    almost bedtime

--- routes/wordRoutes.js
+++ routes/wordRoutes.js
@@ -5,5 +5,5 @@ const wordRoutes = express.Router()
 wordRoutes.post('/search', wordControllers.search)
 wordRoutes.put('/update', wordControllers.update)
 wordRoutes.post('/save', wordControllers.save)
-wordRoutes.get('/findAll', wordControllers.findAll)
+// wordRoutes.get('/findAll', wordControllers.findAll)
 module.exports = wordRoutes
